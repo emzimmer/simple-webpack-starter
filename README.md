c772df13dccc9d897451b8f90dd8586b57711b2b
Max Zimmer <max@emzimmer.com>
2022-04-21 15:56:44 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Simplest Webpack Starter
+
+This only has TWO files to start with. It supplies the bare essentials for compiling/transpiling SCSS and JS.
+* package.json holds the basic package information
+* webpack.config.js holds the generic loader data
+
+**NOTE:** Don't forget to add yarn.lock and node_modules to .gitignore if using version control!!!
+
+Steps to begin the magic!
+1. cd into this folder (_src)
+2. run `yarn`
+3. run `yarn setupScaffolding`
+4. Add some SCSS in _src/scss/main.scss, and some JS is _src/js/main.js
+5. From the _src directory, run `yarn build` to send output to an assets directory that is a sibling of the _src directory
+
+**NOTE:** You can also use `yarn start` to work on the project in dev mode and watch changes.
---
+++ _src/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	// mode: process.env.NODE_ENV,
+	entry: {
+		main: [
+			'./js/main.js',
+			'./scss/main.scss'
+		],
+	},
+	output: {
+		path: path.resolve(__dirname,'../assets'),
+		filename: 'js/[name].min.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					// fallback to style-loader in development
+					// process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].min.css',
+		}),
+	],
+}
